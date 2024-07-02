1b9dedb5199954168436d105dff8d2bc181fdf29
Ali Livan Türk <alilivanturk@gmail.com>
2024-07-02 11:35:35 +0300

    first commit

---
+++ GradientDescentforLogisticRegression.py
@@ -0,0 +1,35 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_common import  dlc, plot_data, plt_tumor_data, sigmoid, compute_cost_logistic
+from plt_quad_logistic import PltQuadLogistic, plt_prob
+plt.style.use('./deeplearning.mplstyle')
+
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train = np.array([0, 0, 0, 1, 1, 1])
+
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X_train, y_train, ax)
+
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.show()
+
+def compute_gradient_descent_logistic(X, y, w, b):
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+    Returns
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b. 
+    """
+
+    m, n = X.shape # m is the number of examples, n is the number of features
+    dj_dw = np.zeros((n,)) # 
+    dj_db = 0.
